7cc48da62c86a38497e2d5fb8c8eee8f8f32a5a5
maksmaggot <maksmaggot@gmail.com>
2020-04-25 17:07:11 +0700

    config

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -2,20 +2,28 @@ package main
 
 import (
 	"github.com/go-chi/chi"
+	"github.com/ilyakaznacheev/cleanenv"
 	"log"
 	"net/http"
+	"workshop/internal/config"
 	"workshop/internal/handler"
 )
 
 func main() {
+	cfg := config.Server{}
+	err := cleanenv.ReadConfig("config.yml", &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	h := handler.NewHandler()
 
 	r := chi.NewRouter()
 
 	r.Get("/hello", h.Hello)
-
-	log.Print("start server")
-	err := http.ListenAndServe(":8080", r)
+	path := cfg.Host + ":" + cfg.Port
+	log.Printf("start server: %s", path)
+	err = http.ListenAndServe(path, r)
 	log.Fatal(err)
 
 	log.Print("shutting down server")
